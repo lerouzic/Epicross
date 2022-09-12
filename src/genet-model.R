01feca1192bd2792d3fe1a2cf23981c1fad6403f
Arnaud Le Rouzic <lerouzic@egce.cnrs-gif.fr>
2022-09-12 17:01:15 +0200

    F2 model from Lynch & Walsh.

--- src/genet-model.R
+++ src/genet-model.R
@@ -17,6 +17,68 @@ model2 <- rbind(
 MODEL <- model2
 
 
+# Design matrices
+Z.2pop.F2 <- function(P1, P2, F1, F2) {
+	nP1 <- length(P1)
+	nP2 <- length(P2)
+	nF1 <- length(F1)
+	nF2 <- length(F2)
+	list(
+		mu = c(rep( 1, nP1), rep( 1, nP2), rep(1, nF1), rep(1, nF2)), 
+		a  = c(rep( 1, nP1), rep(-1,nP2),  rep(0, nF1), rep(0, nF2)), 
+		d  = c(rep(-1, nP1), rep(-1, nP2), rep(1, nF1), rep(0, nF2)), 
+		aa = c(rep( 1, nP1), rep( 1, nP2), rep(0, nF1), rep(0, nF2)), 
+		ad = c(rep(-1, nP1), rep( 1, nP2), rep(0, nF1), rep(0, nF2)), 
+		dd = c(rep( 1, nP1), rep( 1, nP2), rep(0, nF1), rep(0, nF2)))
+}
+
+Z.Npop.Fmu <- function(fP1, fP2, fgen) {
+	stopifnot(length(fP1) == length(fP2), length(fP1) == length(fgen))
+	
+	n <- length(fP1)                # Number of measurements
+	f <- factor(unique(c(fP1,fP2))) # Populations
+	i <- factor(unique(paste(as.character(fP1), as.character(fP2), sep="-")[fP1 != fP2]))
+	
+	mu <- rep(1, length(fP1))
+	a  <- matrix(0, nrow=n, ncol=length(f))
+	d  <- matrix(0, nrow=n, ncol=length(i))
+	aa <- matrix(0, nrow=n, ncol=length(i))
+	dd <- matrix(0, nrow=n, ncol=length(i))
+	
+	for (j in seq_along(fP1)) {
+		fi1 <- which(f == fP1[j])
+		fi2 <- which(f == fP2[j])
+		
+		a[j, fi1] <- a[j, fi1] + 1/2
+		a[j, fi2] <- a[j, fi2] + 1/2
+		
+		if (fP1[j] != fP2[j]) {
+			ii <- which(paste(fP1[j], fP2[j], sep="-") %in% i)
+			aa[j,ii] <- -1
+			if (fgen[j] == "F1") {
+				d[j,ii]  <- 2
+			} else {
+				d[j,ii]  <- 1
+				dd[j,ii] <- -1
+			}
+		}
+	}
+	list(mu=mu, a=a, d=d, aa=aa, dd=dd)
+}
+
+
+fit.2pop.F2 <- function(P1, P2, F1, F2, what=c("a", "d", "aa")) {
+	Z <- do.call(cbind, Z.2pop.F2(P1, P2, F1, F2)[c("mu", what)])
+	lm(c(P1, P2, F1, F2) ~ 0 + Z)
+}
+
+
+fit.Npop.Fmu <- function(fP1, fP2, fgen, phen, what=c("a", "d", "aa") ) {
+	Z <- do.call(cbind, Z.Npop.Fmu(fP1, fP2, fgen)[c("mu", what)])
+	lm(phen ~ 0 + Z)
+}
+
+
 
 fit.2pop <- function(P1, P2, F1, F2) {
 	start <- fit.2pop.noDD(P1, P2, F1, F2)
@@ -37,7 +99,7 @@ fit.2pop.noDD <- function(P1, P2, F1, F2) {
 	ans <- c(
 		zR = (P2+P1)/2,
 		a  = (P2-P1)/2)
-	if (ans["a"] == 0) ans["a"] <- 1e-16
+	if (ans["a"] == 0) ans["a"] <- 1e-6
 	F1s <- (F1-ans["zR"])/ans["a"]
 	F2s <- (F2-ans["zR"])/ans["a"]
 	ds <- 2*(F1s-F2s)
--- src/simu-check1.R
+++ src/simu-check1.R
@@ -33,26 +33,49 @@ plotscross <- function(datacross, xlim=NULL, ylim=NULL, ...) {
 	text(capt.loc,capt.loc/2,"No epistasis", pos=3, srt=28, col="darkviolet")
 }
 
-sim1 <- meancross.multilin(8, 6, a=1:6)
-plotscross(sim1)
-mapply(sim1$P1, sim1$P2, sim1$F1, sim1$F2, FUN=fit.2pop)
+plotcross2 <- function(datacross, xlim=NULL, ylim=NULL, jitter=0.005, maxepi=10, epsilon=FALSE, ...) {
+	effects <- t(mapply(datacross$P1, datacross$P2, datacross$F1, datacross$F2, FUN=fit.2pop))
+	
+	if (epsilon) { 
+		effects[effects[,"e"] < -maxepi ,"e"] <- -maxepi
+		effects[effects[,"e"] >  maxepi ,"e"] <-  maxepi
+	} else {
+		effects[,"e"] <- effects[,"e"] * effects[,"a"]^2
+	}
+	if (is.null(xlim)) xlim <- c(min(min(effects[,"d"]), -0.1), max(max(effects[,"d"]), 0.1))
+	if (is.null(ylim)) ylim <- c(min(min(effects[,"e"]), -0.1), max(max(effects[,"e"]), 0.1))
 
-sim2 <- meancross.multilin(8, 6, a=1:6, d=rnorm(6, 0, 1))
-plotscross(sim2, main="Dominance only")
-mapply(sim2$P1, sim2$P2, sim2$F1, sim2$F2, FUN=fit.2pop)
+	plot(effects[,"d"]+rnorm(nrow(effects), 0, sd=jitter*diff(xlim)), effects[,"e"]+rnorm(nrow(effects), 0, sd=jitter*diff(ylim)), xlim=xlim, ylim=ylim, xlab="Dominance D", ylab=if (epsilon) expression("Epistasis "*epsilon) else expression ("Scaled epistasis a"^2*epsilon), ...)
+	abline(h=0, v=0, col="darkgray")
+	if (epsilon) {
+		if (any(effects[,"e"] == maxepi))
+			abline(h=maxepi, col="darkgray", lty=2)
+		if (any(effects[,"e"] == -maxepi))
+			abline(h=-maxepi, col="darkgray", lty=2)
+	}
+}
 
 
-sim2b <- meancross.multilin(8, 6, a=rnorm(6, 10, 0.1), d=rnorm(6, 5, 0.1))
-plotscross(sim2b)
-md <- mapply(sim2b$P1, sim2b$P2, sim2b$F1, sim2b$F2, FUN=fit.2pop)
+
+nloc <- 6
+npop <- 12
+
+simA <- meancross.multilin(npop, nloc, a=rep(1, nloc))
+plotcross2(simA, main = paste0("Additive, ", nloc, " loci"))
 
 
-sim3 <- meancross.multilin(8, 6, a=1:6, epsilon=rnorm(15, 0, 1))
-plotscross(sim3, main="Epistasis only")
+simD0 <- meancross.multilin(npop, nloc, a=rep(1, nloc), d=seq(-1, 1, length=nloc))
+plotcross2(simD0, main = paste0("Zero-average dominance, ", nloc, " loci"))
 
-sim3b <- meancross.multilin(8, 6, a=rnorm(6,0,0.1), epsilon=rnorm(15, 1, 0.))
-plotscross(sim3b, xlim=c(-1,1), ylim=c(-1,1))
+simDP <- meancross.multilin(npop, nloc, a=rep(1, nloc), d=rep(1, nloc))
+plotcross2(simDP, main = paste0("Positive constant dominance, ", nloc, " loci"))
+
+simDP2 <- meancross.multilin(npop, nloc, a=rep(1, nloc), d=seq(0, 2, length=nloc))
+plotcross2(simDP2, main = paste0("Positive variable dominance, ", nloc, " loci"))
 
 
-sim4 <- meancross.multilin(8, 6, a=1:6, d=rnorm(6, 0, 1), epsilon=rnorm(15, 0, 1))
-plotscross(sim4)
+simE0 <- meancross.multilin(npop, nloc, a=rep(1, nloc), d=rep(0, nloc), e=rnorm(nloc*(nloc-1)/2, 0, 0.1))
+plotcross2(simE0, main = paste0("Zero-average epistasis, ", nloc, " loci"))
+
+simEP <- meancross.multilin(npop, nloc, a=rep(1, nloc), d=rep(0, nloc), e=rep(1, nloc*(nloc-1)/2))
+plotcross2(simEP, main = paste0("Positive constant epistasis, ", nloc, " loci"))
